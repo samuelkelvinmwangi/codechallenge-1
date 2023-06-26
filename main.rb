bec930f9beac30926b5cd328ef8817a05cf0502a
samuel <samuelkelvinmwangi@gmail.com>
2023-06-26 18:36:04 +0300

    isogram

--- main.rb
+++ main.rb
@@ -13,3 +13,21 @@ def calculator(operator, num1, num2)
       nil
     end
   end
+
+  def isIsogram(string):
+    # Convert the string to lowercase to ignore letter case
+    string = string.lower()
+    
+    # Create a set to store the unique letters in the string
+    letters = set()
+    
+    # Iterate through each letter in the string
+    for letter in string:
+        # If the letter is already in the set, it is a repeated letter
+        if letter in letters:
+            return False
+        # Add the letter to the set
+        letters.add(letter)
+    
+    # If no repeated letters were found, it is an isogram
+    return True
